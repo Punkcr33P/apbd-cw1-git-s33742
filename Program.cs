9755a52297025cde8c0a993fac8b8112c57c0d64
Tymur Katalnikov <s33742@pjwstk.edu.pl>
2026-03-15 16:37:27 +0100

    Add header comment in main

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿int[] numbers = { 1, 2, 3, 4, 5 };
+﻿//Commit in first line
+int[] numbers = { 1, 2, 3, 4, 5 };
 static double GetAverage(int[] nums) => nums.Average();
 Console.WriteLine(GetAverage(numbers));
 int max = numbers.Max();
