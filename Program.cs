1ffb81063e3d0a7982a984237cbfdf432370c84c
Tymur Katalnikov <s33742@pjwstk.edu.pl>
2026-03-15 16:35:13 +0100

    Add Max calculation feature

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿int[] numbers = { 1, 2, 3, 4, 5 };
 static double GetAverage(int[] nums) => nums.Average();
 Console.WriteLine(GetAverage(numbers));
+int max = numbers.Max();
+Console.WriteLine($"Max number from numbers: {max}");
